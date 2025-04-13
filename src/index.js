efa006a149d09e925e7dc9abe7ef9e0c7b200611
kimutaifaith <128345240+kimutaifaith@users.noreply.github.com>
2025-04-13 12:26:56 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
      
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App/>
     </BrowserRouter>
   </React.StrictMode>
